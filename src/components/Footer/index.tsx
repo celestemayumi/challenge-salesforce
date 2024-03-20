f2db8d63c2a7387a423252aa428a3780f4d4b05e
Celeste Tanaka <cehmayumi@gmail.com>
2024-03-20 19:39:05 -0300

    🎨 chore: codigo formatado

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,52 +1,56 @@
-import './styles.css'
-import '../../../public/footercloud.png'
-import '../../../public/Logo.png'
-import Image from 'next/image'
+import "./styles.css";
+import "../../../public/footercloud.png";
+import "../../../public/Logo.png";
+import Image from "next/image";
 
-
-
-
-const Footer  = () => {
-    return(
-        <footer>
-            <div className="social">
-                <Image 
-                    src='/Logo.png'
-                    width={190}
-                    height={10}
-                    alt = 'Logo'
-                />
-                <div className="social-icons">
-                    <i className="fa-brands fa-square-facebook"></i>
-                    <i className="fa-brands fa-linkedin"></i>
-                    <i className="fa-brands fa-square-instagram"></i>
-                    <i className="fa-brands fa-square-x-twitter"></i>
-                    <i className="fa-brands fa-square-youtube"></i>
-                </div>
-            </div>
-            <div className="footer-info">
-            <p className='font-bold	'>Ainda nao conhece a Salesforce?</p>
-            <ul>
-                <li><a href=''>Link 1</a></li>
-                <li><a href=''>Link 2</a></li>  
-            </ul>
-            </div>
-            <div className="footer-info">
-            <p className='font-bold	'>Sobre a Salesforce</p>
-            <ul>
-                <li><a href=''>Link 1</a></li>
-                <li><a href=''>Link 2</a></li>   
-            </ul>
-            </div>
-            <div className="footer-info">
-            <p className='font-bold	'>Links populares</p>
-            <ul>
-                <li><a href=''>Link 1</a></li>
-                <li><a href=''>Link 2</a></li>   
-            </ul>
-            </div>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer>
+      <div className="social">
+        <Image src="/Logo.png" width={190} height={10} alt="Logo" />
+        <div className="social-icons">
+          <i className="fa-brands fa-square-facebook"></i>
+          <i className="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-square-instagram"></i>
+          <i className="fa-brands fa-square-x-twitter"></i>
+          <i className="fa-brands fa-square-youtube"></i>
+        </div>
+      </div>
+      <div className="footer-info">
+        <p className="font-bold	">Ainda nao conhece a Salesforce?</p>
+        <ul>
+          <li>
+            <a href="">Link 1</a>
+          </li>
+          <li>
+            <a href="">Link 2</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-info">
+        <p className="font-bold	">Sobre a Salesforce</p>
+        <ul>
+          <li>
+            <a href="">Link 1</a>
+          </li>
+          <li>
+            <a href="">Link 2</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-info">
+        <p className="font-bold	">Links populares</p>
+        <ul>
+          <li>
+            <a href="">Link 1</a>
+          </li>
+          <li>
+            <a href="">Link 2</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,48 +1,51 @@
-import './styles.css'
-import '../../../public/Logo.png'
-import Image from 'next/image'
-import Link from 'next/link'
+import "./styles.css";
+import "../../../public/Logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
-
-const Header = ()=>{
-    return(
-        <>
-        <header>
-            <div className="cloud">
-            <Image 
-                src='/Logo.png'
-                width={280}
-                height={10}
-                alt = 'Logo'
-            />
-            <div className="login">
-                <i className="fas fa-globe config"></i>
-                <button className="login-bar">
-                    <p>Login</p>
-                    <i className="fa-solid fa-user"></i>
-                </button>
-            </div>
-            </div>
-        </header>
-        <nav className='nav'>
-            <ul>
-            <li><Link href="/">Produtos</Link></li>
-            <li><Link href="/">Industrias</Link></li>
-            <li><Link href="/">Suporte</Link></li>
-            <li><Link href="/">Empresa</Link></li>
-            </ul>
-            <div className="search-bar">
-            <input type="text" placeholder="Pesquisar..."/>
-            <button type="submit"><i className="fas fa-search"></i></button>
-            </div>
-        </nav>
-        <div className="acessibility">
-             <p>MENU DE ACESSIBILIDADE</p>
-             <i className ="fas fa-universal-access"></i>
-             
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="cloud">
+          <Image src="/Logo.png" width={280} height={10} alt="Logo" />
+          <div className="login">
+            <i className="fas fa-globe config"></i>
+            <button className="login-bar">
+              <p>Login</p>
+              <i className="fa-solid fa-user"></i>
+            </button>
+          </div>
         </div>
-        </>
-    )
-}
+      </header>
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link href="/">Produtos</Link>
+          </li>
+          <li>
+            <Link href="/">Industrias</Link>
+          </li>
+          <li>
+            <Link href="/">Suporte</Link>
+          </li>
+          <li>
+            <Link href="/">Empresa</Link>
+          </li>
+        </ul>
+        <div className="search-bar">
+          <input type="text" placeholder="Pesquisar..." />
+          <button type="submit">
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+      </nav>
+      <div className="acessibility">
+        <p>MENU DE ACESSIBILIDADE</p>
+        <i className="fas fa-universal-access"></i>
+      </div>
+    </>
+  );
+};
 
 export default Header;
