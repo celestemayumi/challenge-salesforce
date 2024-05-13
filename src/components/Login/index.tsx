c246d93ef7b41dd15fb353085d2b828db9eaf0f4
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-05-13 12:22:25 -0300

    :recycle: refactor: Validação pela API

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import User from "./interfaces";
 import "./styles.css";
@@ -10,24 +10,35 @@ const Login = () => {
   const [senha, setSenha] = useState("");
   const [erro, setErro] = useState("");
   const router = useRouter();
-  
-const handleSubmit = async ()=> {
-    setErro("");
-    const response = await fetch("http://localhost:8080/users", {
-        method: "get"
-     });
-     const data = await response.json();
-    console.log(data);
-     const user = data.find((item: User) => item.email === email && item.senha === senha);
-    console.log(user);
-    if (user) {
-      console.log("Usuário validado");
-      router.push('/logged');
-    } else {
-      setErro("Email ou senha incorretos");
-    }
 
- };
+  const handleSubmit = async () => {
+    setErro("");
+    const response = await fetch("http://localhost:8080/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, senha: senha }),
+    });
+
+    if (!response.ok) {
+      let message;
+      try {
+        message = await response.text();
+        console.log(message);
+        setErro(message);
+      } catch (error) {
+        console.error("Erro ao ler o corpo da resposta:", error);
+        message = "Erro desconhecido ao processar a resposta do servidor.";
+      }
+    } else {
+      const data = await response.text();
+      console.log(data);
+      // tokenService.save(body.data.access_token);
+
+      router.push("/logged");
+    }
+  };
 
   const emailChange = (event: any) => {
     setEmail(event.target.value);
@@ -36,31 +47,38 @@ const handleSubmit = async ()=> {
     setSenha(event.target.value);
   };
 
-
-  return(
+  return (
     <div className="menu">
-    <div></div>
-    <div className="cadastro">
-      <h1>Realizar Login</h1>
-   <div>
-    <label htmlFor="email">Email</label>
-    <input id = "email" onChange={emailChange} type="email" className="border-2" />
-    </div>
-    <div>
-    <label htmlFor="senha">Senha</label>
-    <input id = "senha" onChange={senhaChange} type="password" className="border-2" />
-    </div>
+      <div></div>
+      <div className="cadastro">
+        <h1>Realizar Login</h1>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            onChange={emailChange}
+            type="email"
+            className="border-2"
+          />
+        </div>
+        <div>
+          <label htmlFor="senha">Senha</label>
+          <input
+            id="senha"
+            onChange={senhaChange}
+            type="password"
+            className="border-2"
+          />
+        </div>
 
-    <button 
-    onClick={handleSubmit} 
-    className="bg-slate-500 p-2">
-      Enviar
-    </button>
-    {erro && <p className="error">{erro}</p>}
+        <button onClick={handleSubmit} className="bg-slate-500 p-2">
+          Enviar
+        </button>
+        {erro && <p className="error">{erro}</p>}
+      </div>
+      <div></div>
     </div>
-    <div></div>
-  </div>
-  )
-}
+  );
+};
 
 export default Login;
