f9c32d5865d4813d202e185b60e01cea814041b2
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-05-16 21:25:15 -0300

    :sparkles: feat: atualizar dados do usuario

--- src/components/Logged/index.tsx
+++ src/components/Logged/index.tsx
@@ -13,8 +13,63 @@ function Logged(props: any) {
   const [userTelefone, setUserTelefone] = useState(null);
   const [userEmail, setUserEmail] = useState(null);
   const router = useRouter();
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [empresa, setEmpresa] = useState("");
+  const [resposta, setResposta] = useState("");
+  const [erro, setErro] = useState("");
 
-  const handleClick = async () => {
+  const userUpdate = async () => {
+    setErro("");
+    setResposta("");
+
+    const response = await fetch(
+      "http://localhost:8080/users/" + String(props.id),
+      {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          empresa: empresa,
+          nome: nome,
+          senha: senha,
+          telefone: telefone,
+        }),
+      }
+    );
+    if (!response.ok) {
+      let message;
+
+      try {
+        message = await response.text();
+        //  if(message.)
+        console.log(message);
+        setErro(message);
+      } catch (error) {
+        console.error("Erro ao ler o corpo da resposta:", error);
+        message = "Erro desconhecido ao processar a resposta do servidor.";
+      }
+    } else {
+      let message;
+
+      try {
+        message = await response.text();
+        //  if(message.)
+        console.log(message);
+        setResposta(message);
+      } catch (error) {
+        console.error("Erro ao ler o corpo da resposta:", error);
+        message = "Erro desconhecido ao processar a resposta do servidor.";
+      }
+    }
+    getUserInfos();
+  };
+
+  const getUserInfos = async () => {
     try {
       const response = await fetch(
         "http://localhost:8080/users/" + String(props.id)
@@ -31,7 +86,7 @@ function Logged(props: any) {
   };
 
   useEffect(() => {
-    handleClick();
+    getUserInfos();
   }, []); // Executa apenas uma vez após a montagem do componente
 
   const setPopupClass = () => {
@@ -55,6 +110,21 @@ function Logged(props: any) {
     setPopupClass();
     setClassPopupDelete("popup");
   };
+  const nomeChange = (event: any) => {
+    setNome(event.target.value);
+  };
+  const emailChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const senhaChange = (event: any) => {
+    setSenha(event.target.value);
+  };
+  const telefoneChange = (event: any) => {
+    setTelefone(event.target.value);
+  };
+  const empresaChange = (event: any) => {
+    setEmpresa(event.target.value);
+  };
   const deleteUser = async () => {
     const response = await fetch(
       "http://localhost:8080/users/" + String(props.id),
@@ -72,9 +142,7 @@ function Logged(props: any) {
   return (
     <div className="logged">
       <div className="options">
-        <h1>
-          Bem vindo(a) {userNome}
-        </h1>
+        <h1>Bem vindo(a) {userNome}</h1>
         <button onClick={setPopupView}>Visualizar dados da conta</button>
         <button onClick={setPopupUpdate}>Atualizar dados da conta</button>
         <button onClick={setPopupUpDelete}>Deletar conta</button>
@@ -101,28 +169,57 @@ function Logged(props: any) {
           <div className="popup-content">
             <div>
               <label htmlFor="name">Nome</label>
-              <input id="name" type="text" className="border-2" />
+              <input
+                id="name"
+                type="text"
+                onChange={nomeChange}
+                className="border-2"
+              />
             </div>
 
             <div>
               <label htmlFor="telefone">Telefone</label>
-              <input id="telefone" type="text" className="border-2" />
+              <input
+                id="telefone"
+                type="text"
+                onChange={telefoneChange}
+                className="border-2"
+              />
             </div>
             <div>
               <label htmlFor="empresa">Empresa</label>
-              <input id="empresa" type="text" className="border-2" />
+              <input
+                id="empresa"
+                type="text"
+                onChange={empresaChange}
+                className="border-2"
+              />
             </div>
             <div>
               <label htmlFor="email">Email</label>
-              <input id="email" type="email" className="border-2" />
+              <input
+                id="email"
+                type="email"
+                onChange={emailChange}
+                className="border-2"
+              />
             </div>
             <div>
               <label htmlFor="senha">Senha</label>
-              <input id="senha" type="password" className="border-2" />
+              <input
+                id="senha"
+                type="password"
+                onChange={senhaChange}
+                className="border-2"
+              />
             </div>
             <div className="div-button-update">
-              <button className="button-update">Enviar</button>
+              <button className="button-update" onClick={userUpdate}>
+                Enviar
+              </button>
             </div>
+            {erro && <p className="error">{erro}</p>}
+            {resposta && <p className="success">{resposta}</p>}
           </div>
         </div>
         <div className={`${popupDelete ? "popup" : "none"}`}>
