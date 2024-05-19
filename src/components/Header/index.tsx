8ecc36f22ce623a6722ea7bb1b3942d12e87925c
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-18 21:38:28 -0300

    :sparkles: feat: menu hamburguer na nav bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
 import "./styles.css";
 import "../../../public/Logo.png";
 import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link';
+import { useState } from 'react';
 
 const Header = () => {
+  const [openSubMenu, setOpenSubMenu] = useState<string | null>(null);
+
+  const handleMouseEnter = (menu: string) => {
+    setOpenSubMenu(menu);
+  };
+
+  const handleMouseLeave = () => {
+    setOpenSubMenu(null);
+  };
   return (
     <>
       <header>
@@ -21,27 +31,75 @@ const Header = () => {
         </div>
       </header>
       <nav className="nav">
-        <ul>
-          <li>
-            <Link href="/">Produtos</Link>
-          </li>
-          <li>
-            <Link href="/">Industrias</Link>
-          </li>
-          <li>
-            <Link href="/">Suporte</Link>
-          </li>
-          <li>
-            <Link href="/">Empresa</Link>
-          </li>
-        </ul>
-        <button className="menu-icon"/>
-        <div className="search-bar">
-          <input type="text" placeholder="Pesquisar..." />
-          <button type="submit">
-            <i className="fas fa-search mr-5"></i>
-          </button>
-        </div>
+      <ul className="">
+        <li
+          className="relative"
+          onMouseEnter={() => handleMouseEnter('produtos')}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link href="/">Produtos</Link>
+          {openSubMenu === 'produtos' && (
+            <ul className="absolute flex-col mt-1 z-10 bg-[#0483d7]  text-white">
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/produtos/produto1">Produto 1</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/produtos/produto2">Produto 2</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-28 h-14 "><Link href="/produtos/produto3">Produto 3</Link></li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="relative"
+          onMouseEnter={() => handleMouseEnter('industrias')}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link href="/">Industrias</Link>
+          {openSubMenu === 'industrias' && (
+            <ul className="absolute flex-col mt-1 z-10 bg-[#0483d7]  text-white">
+              <li className="bg-[#0483d7] flex items-center w-32 h-14"><Link href="/industrias/industria1">Indústria 1</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-32 h-14"><Link href="/industrias/industria2">Indústria 2</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-32 h-14"><Link href="/industrias/industria3">Indústria 3</Link></li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="relative"
+          onMouseEnter={() => handleMouseEnter('suporte')}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link href="/">Suporte</Link>
+          {openSubMenu === 'suporte' && (
+            <ul className="absolute flex-col mt-1 z-10 bg-[#0483d7]  text-white">
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/suporte/faq">FAQ</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/suporte/contato">Contato</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/suporte/tutoriais">Tutoriais</Link></li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="relative"
+          onMouseEnter={() => handleMouseEnter('empresa')}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link href="/">Empresa</Link>
+          {openSubMenu === 'empresa' && (
+            <ul className="absolute flex-col mt-1 z-10 bg-[#0483d7]  text-white">
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/empresa/sobre">Sobre Nós</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/empresa/carreiras">Carreiras</Link></li>
+              <li className="bg-[#0483d7] flex items-center w-28 h-14"><Link href="/empresa/contato">Contato</Link></li>
+            </ul>
+          )}
+        </li>
+      </ul>
+      <button className="menu-icon"/>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Pesquisar..."
+          className="p-2 rounded"
+        />
+        <button type="submit" className="">
+          <i className="fas fa-search mr-2"></i>
+        </button>
+      </div>
       </nav>
       <div className="acessibility">
         <p>MENU DE ACESSIBILIDADE</p>
